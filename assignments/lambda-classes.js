18ff368172c6f5fac194d4d6c4aaf894ac1fd1a1
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-04-25 20:29:32 -0400

    Student class created and tested. Not sure about listsSubjects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -62,6 +62,7 @@ class Instructor extends Person{
     }
 }
 
+//Instructors
 const taylor = new Instructor({
     name: 'Taylor',
     age: 24,
@@ -92,6 +93,86 @@ const xavier = new Instructor({
     catchPhrase: "Wildcard, baby!"
 });
 
+
+//Student Class
+class Student extends Person{
+    constructor(studInfo) {
+        super(studInfo);
+        this.previousBackground = studInfo.previousBackground;
+        this.className = studInfo.className;
+        this.favSubjects = studInfo.favSubjects;
+    }
+
+    listsSubjects(){
+        // This feels wrong/too easy. Tried a .foreach() but it returned undefined
+        // this.favSubjects.forEach(function(subject){
+        //    return subject;
+        // });
+        return this.favSubjects;
+    }
+
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}.`;
+    }
+
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}.`;
+    }
+}
+
+//Students
+const neil = new Student ({
+    name: 'Neil',
+    age: 50,
+    location: 'Observatory',
+    gender: 'Male',
+    previousBackground: 'Astronomer',
+    className: 'WEB FT 1',
+    favSubjects: ['HTML', 'CSS', 'JS']
+});
+
+const carl = new Student ({
+    name: 'Carl',
+    age: 70,
+    location: 'Cosmos',
+    gender: 'Male',
+    previousBackground: 'SkyWalker',
+    className: 'Stargazing 101',
+    favSubjects: ['Astronomy', 'Astrology', 'Cosmology']
+});
+
+const bill = new Student ({
+    name: 'Bill',
+    age: 50,
+    location: 'Microsoft',
+    gender: 'Male',
+    previousBackground: 'CEO',
+    className: 'Monopoly',
+    favSubjects: ['Programming', 'Hardware Design', 'Software Design']
+});
+
+
+/* 
+// --- Instructor Testing / demo
 console.log(taylor.demo('Semantic Markup'));
 console.log(ian.demo('Intensive Programming'));
 console.log(xavier.demo('Charlie Work.'));
+
+// --- Instructor testing / grade
+console.log(taylor.grade(neil, 'HTML'));
+console.log(ian.grade(carl, 'Astronomy'));
+console.log(xavier.grade(bill, 'Programming'));
+
+// --- Student testing / listsSubjects
+console.log(neil.listsSubjects());
+console.log(carl.listsSubjects());
+console.log(bill.listsSubjects());
+// -- Student testing PRAssignment
+console.log(neil.PRAssignment('HTML'));
+console.log(carl.PRAssignment('Astronomy'));
+console.log(bill.PRAssignment('Programming'));
+// -- Student testing sprintChallenge
+console.log(neil.sprintChallenge('HTML'));
+console.log(carl.sprintChallenge('Astronomy'));
+console.log(bill.sprintChallenge('Programming'));
+*/
